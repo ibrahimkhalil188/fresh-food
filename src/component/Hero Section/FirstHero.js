07e5e6f21c1ac7127a64be675da8ac52af02a3a5
unknown <ibrahimkhalil.mine@gmail.com>
2022-08-04 13:06:40 +0600

    error solve

--- src/component/Hero Section/FirstHero.js
+++ src/component/Hero Section/FirstHero.js
@@ -2,13 +2,13 @@ import React from 'react';
 import background from '../../Image/greenbackground.jpg';
 import heroImage from '../../Image/fresh-food.webp'
 import './FirstHero.css'
-import Button from '../Shared/Button/Button';
+//import Button from '../Shared/Button/Button';
 const FirstHero = () => {
     return (
-        <div className='hero' style={{ backgroundImage: `url(${background})`, height: '90vh', backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPosition: "center" }}>
+        <div className='hero' style={{ backgroundImage: `url(${background})`, height: '85vh', backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPosition: "center" }}>
 
             <div className='hero-text'>
-                <h1>Fresh Groceries, Meals and more, online delivery.</h1>
+                <h1>Fresh Vegetable, Meals and more, online delivery.</h1>
                 <h3>Best Online Grocery Shop in Bangladesh. We Deliver
                     to your door whatever you want in the local store.</h3>
                 {/* <div className='button'>
