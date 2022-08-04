5e795b9da0e955b531f7939f5c0c4f9c13c141c2
unknown <ibrahimkhalil.mine@gmail.com>
2022-08-04 14:03:15 +0600

    product section added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import FirstHero from '../component/Hero Section/FirstHero';
+import Products from '../component/Products/Products';
 
 const Home = () => {
     return (
         <div>
             <FirstHero></FirstHero>
+            <Products></Products>
         </div>
     );
 };
--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './Products.css'
 const Products = () => {
+    const [products, setProducts] = useState()
+    const url = 'fakedata.json'
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    })
     return (
-        <div>
+        <div className='products-div'>
+            <h1>Our products</h1>
+            {
+                products.map(product =>
+                    <div>
 
+                    </div>)
+            }
         </div>
     );
 };
